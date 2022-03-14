9e17601e68bcc550f6457361f8f1068c334288dc
Sagar Aryal <sagar_yal92@outlook.com>
2022-03-14 16:23:40 +0200

    actions , thunk and middleware applied to complete project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionTypes } from "./states/actions";
 
 function App() {
   const account = useSelector((state) => state.account);
-  console.log(account);
+  //console.log(account);
+  const dispatch = useDispatch();
 
-  return <div>Hello World</div>;
+  const { depositAmount, withdrawAmount } = bindActionCreators(
+    actionTypes,
+    dispatch
+  );
+
+  return (
+    <div>
+      <h1>{account}</h1>
+      <button onClick={() => depositAmount(100)}>Deoposit</button>
+      <button onClick={() => withdrawAmount(100)}>Withdraw</button>
+    </div>
+  );
 }
 
 export default App;
