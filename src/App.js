8160678cbac5813051530447fe31094131ade071
Sagar Aryal <sagar_yal92@outlook.com>
2022-03-14 15:57:09 +0200

    reducers, store and wrapped app with provider completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+  const account = useSelector((state) => state.account);
+  console.log(account);
+
   return <div>Hello World</div>;
 }
 
